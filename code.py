46d2278b89bd40b71663eb42d19d4a395e575cde
Tanvi Rath <rath.t98@gmail.com>
2020-01-12 17:59:57 +0530

    Added: methods for pick_all_numbers,pick_numbers()

--- code.py
+++ code.py
@@ -4,8 +4,10 @@ Load module to filter data on fly
 from __future__ import print_function
 #import os
 #import sys
+import re
 import pandas as pd
-#import numpy as np
+import numpy as np
+
 
 
 class Load(object):
@@ -13,6 +15,7 @@ class Load(object):
     Load class is used for exclusive dataframe filtering
     which takes `df` a csv file and convert into dataframe
     """
+
     def __init__(self, filename):
         self.version = open('VERSION').readlines()[0]
         self.df = pd.read_csv(filename)
@@ -20,7 +23,7 @@ class Load(object):
                         68.40751031,Injuries,Number,Assault,Validated,Whole 
                         pop,All ages,FatalW_A11,2001-03,Moving average,60,10,
                         20,30,33,31,12,51.23477459,68.76522541,Injuries,
-                        Number,Assault,Validated,Whole pop,All ages,Fatale 
+                        Number,Assault,Validated,Whole pop,All ages,Fatale,
                         50, 50, 60,pop,All ages,Fatal"""
 
 
@@ -46,10 +49,18 @@ class Load(object):
             >> [1,2,3,4,5,6]
 
         """
-        print(self.data)
+        data1 = re.sub(r'\s', '', self.data)
+        arr = data1.split(',')
+        global arr3 #pylint: disable=W0601
+        arr3 = []
+        for i in arr:
+            if re.match(r'^[\d.]+[\d]$', i):
+                arr3.append(i)
+        print(arr3)
 
 
-    def sum_all_numbers(self):
+
+    def sum_all_numbers(self):  # pylint: disable=R0201
         """
         From `self.data` extract all numbers and return the sum of all numbers
 
@@ -67,6 +78,11 @@ class Load(object):
 
         """
         # complete code here
+        global n #pylint: disable=W0601
+        n = [float(i) for i in arr3]
+        print(sum(n))
+
+
 
 
     def extract_vowels(self):
@@ -81,9 +97,11 @@ class Load(object):
             >> df.extract_vowels()
             >> ['A', 'E', 'I', 'O']
         """
+        print(re.findall(r'[aeiou]+', self.data))
 
 
-    def pick_odd_numbers(self):
+
+    def pick_odd_numbers(self): # pylint: disable=R0201
         """
         Take the string from `self.data` and extract all odd numbers and return
         list of all odd numbers from the string
@@ -98,9 +116,10 @@ class Load(object):
 
         """
         # complete code here
+        print([ele for ele in n if ele%2 != 0])
 
 
-    def get_mean(self):
+    def get_mean(self): # pylint: disable=R0201
         """
         Take the string from `self.data` and extract all numbers and return
         the mean of extracted list of numbers.
@@ -114,6 +133,7 @@ class Load(object):
             >> 50
         """
         # complete code here
+        print(np.average(n))
 
 
     def get_all_categorical(self):
@@ -131,6 +151,8 @@ class Load(object):
             >> ['Series_reference', 'Type']
         """
         # complete code here
+        print(set(self.df.columns)-set(self.df._get_numeric_data().columns)) # pylint: disable=W0212
+
 
 
 
@@ -149,10 +171,11 @@ class Load(object):
             >> ['Lower_CI', 'Upper_CI', 'Units']
         """
         # complete code here
+        print(set(self.df._get_numeric_data().columns)) # pylint: disable=W0212
 
 
 
-    def addition(self, x, y):
+    def addition(self, x, y): # pylint: disable=R0201
         """
         Take X and Y as input and now return the sum of both
 
@@ -175,4 +198,10 @@ if __name__ == '__main__':
     # instantiate the object
     df = Load('data.csv')
     print(df.addition(2, 3))
-    
+    df.pick_numbers()
+    df.sum_all_numbers()
+    df.extract_vowels()
+    df.pick_odd_numbers()
+    df.get_mean()
+    df.get_all_categorical()
+    df.get_all_continuous()
